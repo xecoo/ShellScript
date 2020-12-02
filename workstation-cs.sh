fe94b3efc66bbf5d60a158725cd7d74668064dac
Bruno Pacheco <bruno@dappbr.com.br>
2020-12-02 10:03:22 -0300

    adicionando sudo apt install nos programas e ealterando a url de alguns arquivos

--- workstation-cs.sh
+++ workstation-cs.sh
@@ -10,18 +10,18 @@ cd /Downloads/programas
 
 ##Google Chrome##
 sudo wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-
-##Discord##
-sudo wget -c https://discordapp.com/api/download?platform=linux&format=deb-O discord.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
 
 ##Postman##
 sudo snap install postman
 
 ##VsCode##
 sudo wget -c https://az764295.vo.msecnd.net/stable/cd9ea6488829f560dc949a8b2fb789f3cdc05f5d/code_1.46.1-1592428892_amd64.deb
+sudo apt install ./code_1.46.1-1592428892_amd64.deb
 
 ##Zoom##
-sudo wget -c https://d11yldzmag5yn.cloudfront.net/prod/5.1.422789.0705/zoom_amd64.deb?_x_zm_rtaid=SeZBFTrqSJSnf07gRJGLXA.1594247414889.1c8953361a6e601c07d545b1ddc1af5b&_x_zm_rhtaid=585
+sudo wget -c https://zoom.us/client/latest/zoom_amd64.deb
+sudo apt install ./zoom_amd64.deb
 
 ##Instalar todos os programas terminados em .deb##
 sudo dpkg -i *.deb
@@ -60,8 +60,6 @@ sudo apt-get install -f -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     ##Autorizar permissões de alteração no arquivo##
     sudo chmod +x /usr/local/bin/docker-compose
-
-    
 
 ##Finalizando a instalação removendo os arquivos desnecessários##
 sudo apt-get autoclean
